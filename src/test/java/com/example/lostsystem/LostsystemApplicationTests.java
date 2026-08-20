a81687877b1c96c295cd7af0f19dd5567e66d9f9
gaoshang <xlbs18291394284@163.com>
2026-08-20 14:54:50 +0800

    Initial commit

---
+++ src/test/java/com/example/lostsystem/LostsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lostsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
